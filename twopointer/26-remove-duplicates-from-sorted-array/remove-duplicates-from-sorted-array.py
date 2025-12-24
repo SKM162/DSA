c689a54d16d48447ed4393debdaf067cce564423
Sai Krishnaraj Mohan <51273744+SKM162@users.noreply.github.com>
2025-12-24 11:16:34 +0530

    Time: 7 ms (15.07%) | Memory: 18.2 MB (98.32%) - LeetSync

---
+++ twopointer/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,28 @@
+# we only have non decreasing order (we won't be swapping to preserve the order), the next will definitely be the next higher unique value.
+# one pointer tracks the position to be replaced in each iteration.
+# one pointer iterates through the array to find next greater.
+
+# let i move through the elements. the current position is always pointing to something to be substituted.
+# replace anything different than the current position. if greater than previous move position.
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        p = 1
+        for i in range(1, len(nums)):
+            # ensure p points to the place to be substituted always.
+            while p < len(nums) and nums[p] > nums[p-1]:
+                p += 1
+            # all elements are unique
+            if p == len(nums):
+                return p
+            
+            if nums[i] != nums[p]:
+                nums[p] = nums[i]
+        # handles moving position for the last value.
+        while p < len(nums) and nums[p] > nums[p-1]:
+                p += 1
+        return p
+
+
+
+
