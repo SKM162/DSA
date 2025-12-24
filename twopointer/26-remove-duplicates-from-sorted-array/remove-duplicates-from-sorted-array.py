0661177077e6943d12fdb486129d569e44d4e8c6
Sai Krishnaraj Mohan <51273744+SKM162@users.noreply.github.com>
2025-12-24 20:12:01 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (99.78%) - LeetSync

--- twopointer/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ twopointer/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,27 +1,15 @@
-# we only have non decreasing order (we won't be swapping to preserve the order), the next will definitely be the next higher unique value.
-# one pointer tracks the position to be replaced in each iteration.
-# one pointer iterates through the array to find next greater.
-
-# let i move through the elements. the current position is always pointing to something to be substituted.
-# replace anything different than the current position. if greater than previous move position.
+# given a non decreasing array, the next greatest element always occurs 
+# when the previous element is lesser. we just need to find the deviations where the element steps up.
+# we can assume only one unique element exists at first at 0
+# only increase the position on finding the next high.
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        p = 1
+        p = 0 
         for i in range(1, len(nums)):
-            # ensure p points to the place to be substituted always.
-            while p < len(nums) and nums[p] > nums[p-1]:
+            if nums[i] > nums[i-1]:
+                #higher element found
                 p += 1
-            # all elements are unique
-            if p == len(nums):
-                return p
-            if nums[i] > nums[p-1]:
                 nums[p] = nums[i]
-        # # handles moving position for the last value.
-        while p < len(nums) and nums[p] > nums[p-1]:
-                p += 1
-        return p
-
-
-
-
+        return p + 1
+        
