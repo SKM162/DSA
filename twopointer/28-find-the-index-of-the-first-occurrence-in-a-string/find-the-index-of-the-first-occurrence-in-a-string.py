f3cf2b94daa54c4833c190158969535980c4547f
Sai Krishnaraj Mohan <51273744+SKM162@users.noreply.github.com>
2025-12-26 10:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (92.00%) - LeetSync

--- twopointer/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
+++ twopointer/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,20 +1,20 @@
-# 28. Find the Index of the First Occurrence in a String - https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/?envType=problem-list-v2&envId=two-pointers
-# find substring in string
-# two pointer - staged
-# left finds the first char of substring
-# right checks full occurrence.
+# find occurrence of substring
+# "" empty is also an occurrence.
+# sliding window - check for all possible starting points
+# first char match and length of substring left in main array.
+
 
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        for i in range(0, len(haystack)):
-            # find match of first char
+        if needle == "":
+            return 0
+        for i in range(0, len(haystack) - len(needle) + 1):
             if haystack[i] == needle[0]:
-                j = 1
-                while j < len(needle) and i + j < len(haystack) and needle[j] == haystack[i+j]:
-                    j += 1
-                if j == len(needle):
+                if haystack[i: i + len(needle)] == needle:
                     return i
         return -1
+
+
                     
 
         
